684eb633c39325e779c645422f7fec8b926ee28b
codception <yusuftalhaklc@gmail.com>
2022-02-08 16:09:53 +0300

    edited code design

---
+++ app/src/main/java/com/yusuftalhaklc/weatherapp/NavigationBarColor.kt
@@ -0,0 +1,18 @@
+package com.yusuftalhaklc.weatherapp
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+
+@Composable
+fun ChangeNavigationBarColor(BarColor:Int){
+    val systemUiController = rememberSystemUiController()
+    var barColor : Color = colorResource(id = BarColor)
+    SideEffect {
+        systemUiController.setNavigationBarColor(
+            color = barColor
+        )
+    }
+}
